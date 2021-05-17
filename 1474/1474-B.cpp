c6ee0422a5197b86c859890db9b578c765c88719
Benjamin <123shuyonghao@gmail.com>
2021-05-16 20:49:10 -0700

    1474-B

---
+++ 1474/1474-B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int d;
+    cin >> d;
+    int p = d + 1;
+    for (; ; p++) {
+        bool is_found = true;
+        for (int j = 2; j * j <= p; j++) {
+            if (p % j == 0) {
+                is_found = false;
+                break;
+            }
+        }
+        if (is_found) {
+            break;
+        }
+    }
+
+    int q = p + d;
+    for (; ; q++) {
+        bool is_found = true;
+        for (int j = 2; j * j <= q; j++) {
+            if (q % j == 0) {
+                is_found = false;
+                break;
+            }
+        }
+        if (is_found) {
+            break;
+        }
+    }
+
+    cout << min(1LL * p * q, 1LL * p * p * p) << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        solve();
+    }
+}
